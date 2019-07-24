13a8282fe7109464efb6014ce7bb1976096a8f66
anand-ia <anand@ioppolo.com.au>
2019-07-24 13:32:49 +0530

    asset_asset

--- ia_asset_float_digits/models/asset_type.py
+++ ia_asset_float_digits/models/asset_type.py
@@ -5,3 +5,12 @@ class AssetType(models.Model):
     _inherit = 'account.asset.category'
 
     method_progress_factor = fields.Float('Degressive Factor',digits=dp.get_precision('asset_decimal'))
+
+
+
+class AssetAsset(models.Model):
+    _inherit = 'account.asset.asset'
+
+    method_progress_factor = fields.Float(string='Degressive Factor', readonly=True, default=0.3,
+                                          states={'draft': [('readonly', False)]},digits=dp.get_precision('asset_decimal'))
+
